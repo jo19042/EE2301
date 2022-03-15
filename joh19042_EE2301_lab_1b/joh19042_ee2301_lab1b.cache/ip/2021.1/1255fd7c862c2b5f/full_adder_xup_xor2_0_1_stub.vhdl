2c805d8b8e0a4cc0a606779eed4e5b74c2d0e7d3
Jacob Johnson <71467906+jo19042@users.noreply.github.com>
2022-03-14 20:57:15 -0500

    Add files via upload

---
+++ joh19042_EE2301_lab_1b/joh19042_ee2301_lab1b.cache/ip/2021.1/1255fd7c862c2b5f/full_adder_xup_xor2_0_1_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.1 (win64) Build 3247384 Thu Jun 10 19:36:33 MDT 2021
+-- Date        : Mon Sep 20 12:28:57 2021
+-- Host        : LAPTOP-CKVJALJQ running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ full_adder_xup_xor2_0_1_stub.vhdl
+-- Design      : full_adder_xup_xor2_0_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    a : in STD_LOGIC;
+    b : in STD_LOGIC;
+    y : out STD_LOGIC
+  );
+
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "a,b,y";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "xup_xor2,Vivado 2021.1";
+begin
+end;
